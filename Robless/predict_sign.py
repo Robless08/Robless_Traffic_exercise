93dce802d8fc3f890f107dfd23f64efd7a06404e
fmiguelulrich <fmiguelulrich@gmail.com>
2025-03-30 20:52:47 +0100

    modified the gui interface

--- Robless/predict_sign.py
+++ Robless/predict_sign.py
@@ -10,6 +10,52 @@ from PIL import Image, ImageTk
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
 
+CATEGORIES = [
+    "Speed limit (20km/h)",
+    "Speed limit (30km/h)",
+    "Speed limit (50km/h)",
+    "Speed limit (60km/h)",
+    "Speed limit (70km/h)",
+    "Speed limit (80km/h)",
+    "End of speed limit (80km/h)",
+    "Speed limit (100km/h)",
+    "Speed limit (120km/h)",
+    "No passing",
+    "No passing for vehicles over 3.5 metric tons",
+    "Right-of-way at the next intersection",
+    "Priority road",
+    "Yield",
+    "Stop",
+    "No vehicles",
+    "Vehicles over 3.5 metric tons prohibited",
+    "No entry",
+    "General caution",
+    "Dangerous curve to the left",
+    "Dangerous curve to the right",
+    "Double curve",
+    "Bumpy road",
+    "Slippery road",
+    "Road narrows on the right",
+    "Road work",
+    "Traffic signals",
+    "Pedestrians",
+    "Children crossing",
+    "Bicycles crossing",
+    "Beware of ice/snow",
+    "Wild animals crossing",
+    "End of all speed and passing limits",
+    "Turn right ahead",
+    "Turn left ahead",
+    "Ahead only",
+    "Go straight or right",
+    "Go straight or left",
+    "Keep right",
+    "Keep left",
+    "Roundabout mandatory",
+    "End of no passing",
+    "End of no passing by vehicles over 3.5 metric tons"
+]
+
 class TrafficSignRecognizer:
     def __init__(self, root, model_file):
         self.root = root
@@ -47,13 +93,13 @@ class TrafficSignRecognizer:
         confidence = np.max(prediction) * 100
         
         img = Image.open(file_path)
-        img = img.resize((200, 200), Image.ANTIALIAS)
+        img = img.resize((200, 200), Image.Resampling.LANCZOS)
         img = ImageTk.PhotoImage(img)
         
         self.image_label.config(image=img)
         self.image_label.image = img
         
-        self.result_label.config(text=f"Predicted Category: {predicted_category}\nConfidence: {confidence:.2f}%")
+        self.result_label.config(text=f"Predicted Category: {CATEGORIES[predicted_category]}\nConfidence: {confidence:.2f}%")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
